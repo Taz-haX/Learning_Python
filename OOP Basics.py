b1371bdfaa4fd6833749f60dd5259a0ee7e9e9f2
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-01-26 21:40:28 +0600

    Add files via upload

--- OOP Basics.py
+++ OOP Basics.py
@@ -1,20 +1,35 @@
 #
 class Item:
+
+    def __init__(self, nama: str, races = 0, podiums = 0):
+        assert races >= 0, f'No. of races {races} below zero'
+        assert podiums >= 0
+
+        self.race = races
+        self.podium = podiums
+        self.nam = nama
+
+
     def calculate_championship(self, a, b):
         return a * b
 
+    def calculate_podium_percentage(self):
+        return ((self.podium / self.race)*100)
 
-item1 = Item()
+
+item1 = Item('Hammertime', 300, 103)
 
 item1.name = 'Pajn'
 item1.price = 44
 item1.quantity = 7
 print(item1.calculate_championship(item1.price, item1.quantity))
+print(item1.calculate_podium_percentage())
+print(item1.nam)
 
 # print(type(item1))
 # print(type(item1.quantity))
 
-item2 = Item()
+item2 = Item('verr')
 
 item2.name = 'Stappen'
 item2.price = 1
