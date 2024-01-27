5c03336705f4859eed8d1287012366a32624c7f6
Xabhier Hayat Tazwar <155357017+Taz-haX@users.noreply.github.com>
2024-01-27 21:57:29 +0600

    Add files via upload

--- OOP Basics.py
+++ OOP Basics.py
@@ -1,6 +1,8 @@
 #
 class Item:
 
+    pay_rate = 0.8
+
     def __init__(self, nama: str, races = 0, podiums = 0):
         assert races >= 0, f'No. of races {races} below zero'
         assert podiums >= 0
@@ -16,6 +18,11 @@ class Item:
     def calculate_podium_percentage(self):
         return ((self.podium / self.race)*100)
 
+    def apply_discount(self):
+        self.podium = self.podium * Item.pay_rate
+        return self.podium
+
+
 
 item1 = Item('Hammertime', 300, 103)
 
@@ -25,6 +32,12 @@ item1.quantity = 7
 print(item1.calculate_championship(item1.price, item1.quantity))
 print(item1.calculate_podium_percentage())
 print(item1.nam)
+# print(Item.pay_rate)
+# print(item1.pay_rate)
+# print(Item.__dict__)
+# print(item1.__dict__)
+print(item1.apply_discount())
+print(item1.podium)
 
 # print(type(item1))
 # print(type(item1.quantity))
